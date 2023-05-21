aac8385afc16d9bf43887ff709acf4edc38ef619
Aaron Barnaby <aaron.barnaby@funkypigeon.com>
2023-05-21 22:45:05 +0100

    re-add about page

--- components/Layout/TopNavbar/TopNavbar.tsx
+++ components/Layout/TopNavbar/TopNavbar.tsx
@@ -20,7 +20,7 @@ export function TopNavbar() {
       <nav className="z-10 hidden sm:flex md:inset-0 md:justify-center">
         <div className="flex items-center md:gap-2">
           <NavItem href="/home" text="Home" />
-          {/* <NavItem href="/about" text="About" /> */}
+          <NavItem href="/about" text="About" />
           {/* <NavItem href="/projects" text="Projects" /> */}
           {/* <NavItem href="/skills" text="Skills" /> */}
         </div>
